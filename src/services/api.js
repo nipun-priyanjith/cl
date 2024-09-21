ad6bd857048999de6cda160116e459f3c2cb018f
Nipun Prijanjith <133010944+nipun-priyanjith@users.noreply.github.com>
2024-09-21 11:14:46 +0530

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -2,37 +2,6 @@
 
 const API_URL = 'https://nipun-priyanjith-il95f5x4w-nipuns-projects-a980ca2d.vercel.app/api';// Ensure /api prefix matches your server routes
 
-// export const fetchWords = async () => {
-//   try {
-//     const response = await fetch(`${API_URL}/words`);
-//     if (!response.ok) {
-//       throw new Error('Network response was not ok');
-//     }
-//     return await response.json();
-//   } catch (error) {
-//     console.error('Fetch error:', error);
-//     throw error;
-//   }
-// };
-
-// export const createWord = async (word) => {
-//   try {
-//     const response = await fetch(`${API_URL}/words`, {
-//       method: 'POST',
-//       headers: {
-//         'Content-Type': 'application/json',
-//       },
-//       body: JSON.stringify(word),
-//     });
-//     if (!response.ok) {
-//       throw new Error('Network response was not ok');
-//     }
-//     return await response.json();
-//   } catch (error) {
-//     console.error('Create word error:', error);
-//     throw error;
-//   }
-// };
 
 
 
@@ -70,20 +39,7 @@ export const fetchExperiences = async () => {
 };
 
 
-// client/src/services/api.js
-// cccccccccccccccccccccccc
-// export const fetchProjects = async () => {
-//   try {
-//     const response = await fetch(`${API_URL}/projects`); // Ensure /projects matches your server route
-//     if (!response.ok) {
-//       throw new Error(`Network response was not ok: ${response.statusText}`);
-//     }
-//     return await response.json();
-//   } catch (error) {
-//     console.error('Fetch error:', error);
-//     throw error;
-//   }
-// };
+
 
 
 
