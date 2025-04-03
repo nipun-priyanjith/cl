c19f18aa8787f4894458030fa263d0efa3e8b818
Nipun Prijanjith <133010944+nipun-priyanjith@users.noreply.github.com>
2025-04-03 06:32:48 +0530

    Update index.js

--- src/constants/index.js
+++ src/constants/index.js
@@ -2,7 +2,7 @@
 
 export const HERO_CONTENT = ` Driven and skilled DevOps Engineer pursuing a BSc (Hons) in Computing at NIBM, with hands-on expertise in cloud infrastructure, automation, and CI/CD pipelines. I specialize in Kubernetes, Terraform, Ansible, Jenkins, AWS, Firebase, Docker, Grafana, and other cutting-edge DevOps tools. Passionate about automating workflows, optimizing deployments, and building scalable, resilient systems. Always eager to push the boundaries of cloud and DevOps innovation.  `;
 
-export const ABOUT_TEXT = `I'm an undergraduate software engineering student who enjoys breaking down complex problems into smaller parts and solving them effectively. I aim to leverage robust programming skills and continuous learning to contribute to software development, collaborate effectively with experienced engineers, and ensure team success in meeting project deadlines.`;
+export const ABOUT_TEXT = `I'm an undergraduate software engineering student who enjoys breaking down complex infrastructure challenges into scalable and automated solutions. I aim to leverage strong skills in cloud computing, CI/CD, and infrastructure as code to optimize deployment processes, collaborate with experienced engineers, and ensure seamless operations and system reliability. `;
 
 
 
