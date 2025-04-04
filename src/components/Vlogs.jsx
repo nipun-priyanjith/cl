4a7ffc7864b27e249348a62b05c198eeea8a9f93
Nipun Prijanjith <133010944+nipun-priyanjith@users.noreply.github.com>
2025-04-04 07:01:11 +0530

    Update Vlogs.jsx

--- src/components/Vlogs.jsx
+++ src/components/Vlogs.jsx
@@ -7,7 +7,7 @@ const articles = [
     link: "https://www.reddit.com/r/programming/comments/...",
     icon: <FaReddit className="text-[#FF4500] text-3xl" />, 
     description: "A deep dive into using Git effectively for version control and collaboration.",
-    image: "https://via.placeholder.com/150"
+    image: "https://th.bing.com/th/id/OIP.KOgu6qiko5ct4HlbVXr3HAHaD9?rs=1&pid=ImgDetMain"
   },
   {
     title: "Building a Malware Detection System in Python",
@@ -15,7 +15,7 @@ const articles = [
     link: "https://medium.com/@yourusername/building...",
     icon: <FaMedium className="text-[#000000] text-3xl" />, 
     description: "Step-by-step guide on developing a malware detection system using machine learning.",
-    image: "https://via.placeholder.com/150"
+    image: "https://th.bing.com/th/id/OIP.nuNA-Oo4Up98qI5Wc_PulAHaEK?w=1280&h=720&rs=1&pid=ImgDetMain"
   },
   {
     title: "How to Land a Software Engineering Internship",
@@ -23,7 +23,7 @@ const articles = [
     link: "https://www.linkedin.com/pulse/how-to-land...",
     icon: <FaLinkedin className="text-[#0077B5] text-3xl" />, 
     description: "Essential tips and strategies to secure a software engineering internship.",
-    image: "https://via.placeholder.com/150"
+    image: "https://th.bing.com/th/id/R.ece71c13d5169f86feecf697b4acdfbf?rik=oHn4KYyf8FH8VA&riu=http%3a%2f%2fcdn3.whatculture.com%2fwp-content%2fuploads%2f2015%2f05%2fR1jzAvZI.jpg&ehk=lNuBvGYRfV44%2fiPREF3fwze%2bIpVtNOLMLoeqdCbnR6o%3d&risl=&pid=ImgRaw&r=0"
   },
 ];
 
